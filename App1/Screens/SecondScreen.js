15cb4755e58eab962df2a0539502dc7fbaf85133
OmerCopilot <omer_yilmazoglu@gmx.de>
2025-04-06 18:11:55 +0200

    complete first design

--- App1/Screens/SecondScreen.js
+++ App1/Screens/SecondScreen.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
 import Joystick from '../Components/Joystick';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MainMap from '../Maps/MainMap';
 import MainCharacter from '../Characters/MainCharacter';
+
+const { width, height } = Dimensions.get('window');
+
 const SecondScreen = () => {
-  const [characterPosition, setCharacterPosition] = useState({ x: 150, y: 150 });
+    const [characterPosition, setCharacterPosition] = useState({ x: width / 2 - 25, y: height / 2 - 25});
 
   const handleJoystickMove = (dx, dy) => {
     // Move the character continuously based on joystick direction
